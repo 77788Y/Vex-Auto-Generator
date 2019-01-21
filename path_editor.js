d2fe64150ac6f0bd4c2dfa63fa1ca0a84c605073
ZachDaChampion <zachchampion79@gmail.com>
2019-01-20 23:02:06 -0500

    doesn't draw tentative point if moving

--- path_editor.js
+++ path_editor.js
@@ -1,26 +1,29 @@
 let next_point = new Point();
+let draw_tentative = true;
 
 function run_path_manager() {
-  
-  next_point = new Point(mouseX / field_size, (mouseY - 96) / field_size);
-  next_point.is_tentative = true;
 
   let pts = copy_array(path_points);
   let mvs = copy_array(path_moves);
+  
+  if (draw_tentative) {
+    next_point = new Point(mouseX / field_size, (mouseY - 96) / field_size);
+    next_point.is_tentative = true;
 
-  if (selected == 'field_point') pts.splice(selected_index + 1, 0, next_point);
-  else pts.push(next_point);
+    if (selected == 'field_point') pts.splice(selected_index + 1, 0, next_point);
+    else pts.push(next_point);
 
-  if (path_points.length > 0) {
+    if (path_points.length > 0) {
 
-    let move = selected_tool == 'line' ? new Line() : (
-      selected_tool == 'arc' ? new Arc() : null
-    );
+      let move = selected_tool == 'line' ? new Line() : (
+        selected_tool == 'arc' ? new Arc() : null
+      );
 
-    if (keyIsDown(CONTROL) && move.type == 'arc') move.use_large = true;
+      if (keyIsDown(CONTROL) && move.type == 'arc') move.use_large = true;
 
-    if (selected == 'field_point')  mvs.splice(selected_index + 1, 0, move);
-    else mvs.push(move);
+      if (selected == 'field_point')  mvs.splice(selected_index + 1, 0, move);
+      else mvs.push(move);
+    }
   }
   
   draw_path_moves(pts, mvs);
@@ -30,8 +33,12 @@ function run_path_manager() {
 function mouseDragged() {
 
   if (selected == 'field_point') {
+    draw_tentative = false;
     path_points[selected_index].x = mouseX / field_size;
     path_points[selected_index].y = (mouseY - 96) / field_size;
   }
+}
 
+function mouseReleased() {
+  draw_tentative = true;
 }

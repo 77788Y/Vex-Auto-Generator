312326812908c997d368372348bd7a82eced98f8
ZachDaChampion <zachchampion79@gmail.com>
2019-01-21 22:36:37 -0500

    added save/generate buttons, useless for now

--- save_load.js
+++ save_load.js
@@ -24,3 +24,17 @@ function save_path() {
     path.mvs = path_moves;
     return path;
 }
+
+let text_file = null;
+function save_to_file(text) {
+
+    let data = new Blob([text], {type: 'text/plain'});
+    if (text_file != null) window.URL.revokeObjectURL(text_file);
+    text_file = window.URL.createObjectURL(data);
+
+    let save_btn = document.getElementById("btn_save");
+    save_btn.setAttribute('download', 'auton_path.txt');
+    save_btn.href = text_file;
+    
+    return text_file;
+}
